c345956227ea5e6bf8747ca9cfd983e413255e6a
amancho <amanchosalesh@gmail.com>
2021-06-04 22:58:49 +0200

    Create basic UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoItem from './TodoItem.js';
+import TodoForm from './TodoForm.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Todo application</h1>
+      <TodoForm />
+
+      <div className="todo-list">
+        <TodoItem title="First item"/>
+        <TodoItem title="Second item" isCompleted/>
+        <TodoItem title="Third item" isCompleted />
+      </div>
     </div>
   );
 }
---
+++ client/src/TodoForm.js
@@ -0,0 +1,9 @@
+function TodoForm(){
+    return (
+        <form className="todo-form">
+            <input type="text" name="title" placeholder="Add a new task"></input>
+        </form>
+    );
+}
+
+export default TodoForm
